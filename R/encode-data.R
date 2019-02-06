e9e3eb715b1718fa4f437d98890fac2352ba3eb2
shansabri <shanasabri@gmail.com>
2019-02-05 16:01:13 -0800

    upd documentation + example src

--- R/encode-data.R
+++ R/encode-data.R
@@ -5,18 +5,12 @@
 #' of chr16 (491038 non-overlapping 200bp bins).
 #'
 #' @docType data
-#'
 #' @usage data(encode_signal)
-#'
 #' @format An object of class \code{"GRanges"}; see \code{\link[GenomicRanges]{makeGRangesFromDataFrame}}.
-#'
 #' @keywords datasets
-#'
 #' @references Yue et al. (2014) Nature 515:355-364
 #' (\href{https://www.ncbi.nlm.nih.gov/pubmed/25409824}{PubMed})
-#'
 #' @source \href{http://www.mouseencode.org}{ENCODE Mouse Project}
-#'
 #' @examples
 #' data(encode_signal)
 "encode_signal"
@@ -29,18 +23,12 @@
 #' TPM values with replicates aggregrated for 14539 genes.
 #'
 #' @docType data
-#'
 #' @usage data(encode_expr)
-#'
 #' @format An object of class \code{"data.frame"}.
-#'
 #' @keywords datasets
-#'
 #' @references Yue et al. (2014) Nature 515:355-364
 #' (\href{https://www.ncbi.nlm.nih.gov/pubmed/25409824}{PubMed})
-#'
 #' @source \href{http://www.mouseencode.org}{ENCODE Mouse Project}
-#'
 #' @examples
 #' data(encode_expr)
 "encode_expr"
--- example/example.R
+++ example/example.R
@@ -14,6 +14,7 @@ library(deconR)
 # Load ENCODE data
 data(encode_signal)
 data(encode_expr)
+data_split <- readRDS(file.path("~/Dropbox/ErnstLab/decon_mouse", "data", "DATA-SPLIT.rds"))
 
 
 
@@ -31,10 +32,19 @@ scrna_aggr <- scrna_aggr[row.names(encode_expr), paste("Cluster", clusters_of_in
 
 
 
+# Subset to differential genes (OPTIONAL)
+diff_exp <- read.table(gzfile("~/Dropbox/PlathLab/Analyses/Justin_Langerman/Timecourse_analysis/Clustering/Drop7/2016-12-21-final-data/diffexp/Drop7.negbinom.cluster.v.all.sig.genes.tsv.gz"), header=TRUE)
+diff_exp_genes <- as.vector(subset(diff_exp, cluster %in% c("C1", "C23"))$gene)
+encode_expr <- encode_expr[row.names(encode_expr) %in% diff_exp_genes, ]
+scrna_aggr <- scrna_aggr[row.names(scrna_aggr) %in% diff_exp_genes, ]
+identical(row.names(scrna_aggr), row.names(encode_expr))
+
+
+
 # How well do these data correlate (ENCODE vs aggregrated scRNA)?
 corrs <- list(p_cor = cor(cbind.data.frame(scrna_aggr, encode_expr), method = "pearson")[,1:2],
               s_cor = cor(cbind.data.frame(scrna_aggr, encode_expr), method = "spearman")[,1:2])
-lapply(seq_along(corrs), function(x){
+heatmaps <- lapply(seq_along(corrs), function(x){
   ph <- pheatmap::pheatmap(corrs[[x]],
                      display_numbers = TRUE,
                      cluster_cols = FALSE,
@@ -46,6 +56,28 @@ lapply(seq_along(corrs), function(x){
 
 
 
+# Create in silico convolved signal track
+MEF <- read_signal("~/Dropbox/ErnstLab/deconvolution_old/CHIP/signal/full/200bp-windows/liftOver-mm10/MEF_K27AC.sorted.wig", format = "wig", chr = "chr16")
+ESC <- read_signal("~/Dropbox/ErnstLab/deconvolution_old/CHIP/signal/full/200bp-windows/liftOver-mm10/ES_K27AC.sorted.wig", format = "wig", chr = "chr16")
+convolved <- make_convolved(list(ct1 = MEF, ct2 = ESC), log2_scale = TRUE)
+
+
+
+# Cluster loci to reduce dimensionality *only cluster training data*
+n_clusters <- 500
+clusters <- cluster_loci(encode_signal[, data_split$TRAINING], k = n_clusters)
+
+
+
+# Train across-cell type models *one model per cluster*
+across_model_output <- model_across_celltypes(clusters, scrna_aggr, encode_signal, encode_expr,
+                                              data_split, genes = diff_exp_genes, seed = 42,
+                                              output_dir = "~/Dropbox/ErnstLab/deconR/example/ACROSS-MODEL-PLOTS")
+
+
+
+
+
 
 
 
@@ -53,3 +85,4 @@ lapply(seq_along(corrs), function(x){
 
 # NOTES
 # save(encode_signal, file = file.path("~/Dropbox/ErnstLab/deconR/data", "encode_signal.RData"), compress='xz')
+# encode_signal_metadata <- cbind.data.frame(chr = seqnames(encode_signal), ranges(encode_signal))
